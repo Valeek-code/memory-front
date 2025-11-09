8b4925aab549508bcde550a32c8bd61d54fe6573
xant1ko <semasavasa@gmail.com>
2025-11-09 22:08:37 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+<!-- header TODO -->
+<MainPage/>
+</template>
+
+<script lang="ts" setup>
+import MainPage from '@/components/MainPage.vue';
+
+
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+    <v-container class="today-task-card">
+    <p>dsd</p>
+    </v-container>
+
+
+</v-container>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.today-task-card{
+background-color: #272E36;
+border-radius: 10px;
+width: 60vw;
+}
+</style>
