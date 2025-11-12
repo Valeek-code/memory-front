398acdbb10a8b3bb724de893310489e0bd4db0ba
xant1ko <semasavasa@gmail.com>
2025-11-12 17:02:10 +0700

    feat: header fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,39 @@
 <template>
-  <v-container class="header">
-    <v-row />
-  </v-container>
+  <v-row class="header">
+    <v-container class="icon-container">
+      <img class="icon" src="@/assets/planer_icon.svg" />
+    </v-container>
+    <v-container class="icon-container">
+      <img class="icon" src="@/assets/notes_icon.svg" />
+    </v-container>
+    <v-container class="icon-container">
+      <img class="icon" src="@/assets/files_icon.svg" />
+    </v-container>
+  </v-row>
 </template>
 
 <script>
 
 </script>
+
+<style scoped>
+.header {
+  padding: 7px;
+  height: 65px;
+}
+
+.icon {
+  width: 40px;
+  margin: 0%;
+  color: transparent;
+}
+
+.icon-container {
+  /* background: aqua;
+overflow:hidden; */
+  padding: 0px;
+  max-width: 40px;
+  margin: 5px;
+  height: 40px;
+}
+</style>
--- src/components/pages/NotesPage.vue
+++ src/components/pages/NotesPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <p style="text-align: center;">NotesPage</p>
-  <v-row class="justify">
-    <p> Когда на сайт заходит не зарегестирированный пользователь, у него выскакивает страница такого содержания <br> Предложение войти или зарегестрироватся <br> общий топ заметок. </p>
+  <v-container>
+  <p class="ma-10" style="text-align: center;">NotesPage</p>
+  <v-row class="ma-10 justify-center" >
+    <p style="text-align: center;">
+      Когда на сайт заходит не зарегестирированный пользователь, у него выскакивает страница такого содержания <br> 
+      Предложение войти или зарегестрироватся <br>
+      общий топ заметок. </p>
   </v-row>
+  <v-row class="justify-center ma-10">
+    <p style="text-align: center;">Спросить у сереги за изменение цвета svg</p>
+  </v-row>
+  </v-container>
 </template>
